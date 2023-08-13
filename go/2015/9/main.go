5cb34a61c498bc35e981970a889178f78d384032
Charles Palsa <20684277+cpalsa@users.noreply.github.com>
2023-08-13 00:23:36 -0400

    more util funcs+refactor, finish day 9

--- go/2015/9/main.go
+++ go/2015/9/main.go
@@ -1,16 +1,130 @@
 package main
 
 import (
+	"errors"
 	"fmt"
+	"strconv"
+	"strings"
 
 	"github.com/cpalsa/advent-of-code/go/util"
 )
 
 const inputFile = "input.txt"
 
+type path struct {
+	dest string
+	dist int
+}
+
+func parsePaths(input []byte) map[string][]path {
+	paths := make(map[string][]path)
+
+	for _, p := range strings.Split(string(input), "\n") {
+		pieces := strings.Split(p, " ")
+		start := pieces[0]
+		end := pieces[2]
+		dist, err := strconv.Atoi(pieces[4])
+		if err != nil {
+			util.HandleFatal(err)
+		}
+
+		// make sure both location mappings exist and have their list of destinations initialized
+		if _, ok := paths[start]; !ok {
+			paths[start] = []path{}
+		}
+		if _, ok := paths[end]; !ok {
+			paths[end] = []path{}
+		}
+
+		paths[start] = append(paths[start], path{end, dist})
+		paths[end] = append(paths[end], path{start, dist})
+	}
+
+	return paths
+}
+
+func getShortestPath(paths []path) path {
+	if len(paths) == 0 {
+		util.HandleFatal(errors.New("no paths to compare"))
+	}
+	if len(paths) == 1 {
+		return paths[0]
+	}
+
+	shortest := paths[0]
+	for _, path := range paths {
+		if path.dist < shortest.dist {
+			shortest = path
+		}
+	}
+
+	return shortest
+}
+
+func getLongestPath(paths []path) path {
+	if len(paths) == 0 {
+		util.HandleFatal(errors.New("no paths to compare"))
+	}
+	if len(paths) == 1 {
+		return paths[0]
+	}
+
+	longest := paths[0]
+	for _, path := range paths {
+		if path.dist > longest.dist {
+			longest = path
+		}
+	}
+
+	return longest
+}
+
 func main() {
 	input, err := util.LoadInput(inputFile)
 	util.HandleFatal(err)
 
-	fmt.Printf("%v\n", string(input))
+	paths := parsePaths(input)
+
+	// Part 1
+	shortestRoutes := make(map[string][]path)
+	shortestDistances := make(map[string]int)
+	for start, choices := range paths {
+		shortestRoutes[start] = []path{}
+		visited := []string{start}
+
+		for len(choices) != 0 {
+			// get the shortest path from our choices
+			shortest := getShortestPath(choices)
+			shortestRoutes[start] = append(shortestRoutes[start], shortest)
+			shortestDistances[start] += shortest.dist
+			visited = append(visited, shortest.dest)
+
+			// filter available choices of routes by the names of places we already visited
+			choices = util.FilterSlice(paths[shortest.dest], func(i int, v path) bool {
+				return !util.SliceContains(visited, v.dest)
+			})
+		}
+	}
+	fmt.Printf("(Part 1) The shortest route Santa can take is a distance of %v\n", util.Min(util.MapVals(shortestDistances)...))
+
+	longestRoutes := make(map[string][]path)
+	longestDistances := make(map[string]int)
+	for start, choices := range paths {
+		longestRoutes[start] = []path{}
+		visited := []string{start}
+
+		for len(choices) != 0 {
+			// get the longest path from our choices
+			longest := getLongestPath(choices)
+			longestRoutes[start] = append(longestRoutes[start], longest)
+			longestDistances[start] += longest.dist
+			visited = append(visited, longest.dest)
+
+			// filter available choices of routes by the names of places we already visited
+			choices = util.FilterSlice(paths[longest.dest], func(i int, v path) bool {
+				return !util.SliceContains(visited, v.dest)
+			})
+		}
+	}
+	fmt.Printf("(Part 2) The longest route Santa can take is a distance of %v\n", util.Max(util.MapVals(longestDistances)...))
 }
--- go/util/util.go
+++ go/util/util.go
@@ -27,9 +27,50 @@ func HandleFatal(err error) {
 	}
 }
 
+// Iterates over the input slice, passing the index and value to the callback function for each iteration.
+// Returns a new slice containing only values that pass the callback predicate (return true)
+func FilterSlice[T any](s []T, cb func(i int, v T) bool) []T {
+	// Not sure how to make this more generic to also handle maps
+	filtered := []T{}
+	for i, v := range s {
+		if cb(i, v) {
+			filtered = append(filtered, v)
+		}
+	}
+
+	return filtered
+}
+
+// Iterates over the input map, passing the key and value to the callback function for each iteration.
+// Returns a new map containing only key+value pairs that pass the callback predicate (return true)
+func FilterMap[K comparable, V any](m map[K]V, cb func(k K, v V) bool) map[K]V {
+	// Not sure how to make this more generic to also handle slices
+	filtered := make(map[K]V)
+
+	for k, v := range m {
+		if cb(k, v) {
+			filtered[k] = v
+		}
+	}
+
+	return filtered
+}
+
+// Iterates over the input array, passing the index and value to the callback function for each iteration.
+// Returns a new array built from the return values of the callback function.
+func Map[T any, M any](s []T, cb func(i int, v T) M) []M {
+	mapped := []M{}
+
+	for i, v := range s {
+		mapped = append(mapped, cb(i, v))
+	}
+
+	return mapped
+}
+
 func PrintSlice[T any](s []T) {
 	// Not sure how to make this generic to only range-able to also handle maps
-	fmt.Println("slice[")
+	fmt.Println("[")
 	for k, v := range s {
 		fmt.Printf("  %v: %+v,\n", k, v)
 	}
@@ -38,26 +79,93 @@ func PrintSlice[T any](s []T) {
 
 func PrintMap[K comparable, V any](m map[K]V) {
 	// Not sure how to make this generic to only range-able to also handle slices
-	fmt.Println("map{")
+	fmt.Println("{")
 	for k, v := range m {
 		fmt.Printf("  %v: %+v,\n", k, v)
 	}
 	fmt.Println("}")
 }
 
-// Returns the minimum value of given Orderables
-// DEPRECATED -- go 1.21 also introduced min/max funcs
-func Min[T cmp.Ordered](vals ...T) (min T, err error) {
-	if len(vals) == 0 {
-		return min, errors.New("no comparable values")
-	}
-
-	min = vals[0]
-	for _, val := range vals {
-		if val < min {
-			min = val
+// Iterates over a slice to see if the given value exists within the slice.
+func SliceContains[T comparable](s []T, v T) bool {
+	for _, sv := range s {
+		if sv == v {
+			return true
 		}
 	}
 
-	return min, nil
+	return false
+}
+
+// Iterates over a map to see if the given value exists within the map.
+func MapContains[K comparable, V comparable](m map[K]V, v V) bool {
+	for _, mv := range m {
+		if mv == v {
+			return true
+		}
+	}
+
+	return false
+}
+
+// Returns a slice containing all keys of the given map
+func MapKeys[K comparable, V any](m map[K]V) []K {
+	keys := []K{}
+
+	for k := range m {
+		keys = append(keys, k)
+	}
+
+	return keys
+}
+
+// Returns a slice containing all values of the given map
+func MapVals[K comparable, V any](m map[K]V) []V {
+	vals := []V{}
+
+	for _, v := range m {
+		vals = append(vals, v)
+	}
+
+	return vals
+}
+
+// Returns the minimum value respective to the type of the values given
+func Min[T cmp.Ordered](vals ...T) T {
+	if len(vals) == 0 {
+		HandleFatal(errors.New("Min: no values to compare"))
+	}
+
+	if len(vals) == 1 {
+		return vals[0]
+	}
+
+	min := vals[0]
+	for _, v := range vals {
+		if v < min {
+			min = v
+		}
+	}
+
+	return min
+}
+
+// Returns the maximum value respective to the type of the values given
+func Max[T cmp.Ordered](vals ...T) T {
+	if len(vals) == 0 {
+		HandleFatal(errors.New("Max: no values to compare"))
+	}
+
+	if len(vals) == 1 {
+		return vals[0]
+	}
+
+	max := vals[0]
+	for _, v := range vals {
+		if v > max {
+			max = v
+		}
+	}
+
+	return max
 }
